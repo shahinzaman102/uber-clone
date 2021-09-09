ee3e475cfcd6b3ffbd07959a193760ceb633acf5
Shahin Zaman <shahinzaman@Macbooks-MacBook-Air.local>
2021-09-10 04:22:08 +0600

    inital commit

---
+++ screens/MapScreen.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import tw from 'tailwind-react-native-classnames'
+import Map from '../components/Map'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import NavigateCard from '../components/NavigateCard'
+import RideOptionsCard from '../components/RideOptionsCard'
+import { Icon } from 'react-native-elements/dist/icons/Icon'
+import { useNavigation } from '@react-navigation/core'
+
+const MapScreen = () => {
+    const Stack = createNativeStackNavigator();
+    const navigation = useNavigation();
+
+    return (
+        <View>
+
+            <TouchableOpacity
+                onPress={() => navigation.navigate("HomeScreen")}
+                style={tw`bg-gray-100 absolute top-16 left-8 z-50 p-3 rounded-full shadow-lg`}
+            >
+                <Icon name="menu" />
+            </TouchableOpacity>
+
+            <View style={tw`h-1/2`}>
+                <Map />
+            </View>
+
+            <View style={tw`h-1/2`}></View>
+
+            <Stack.Navigator>
+                <Stack.Screen
+                    name="navigateCard"
+                    component={NavigateCard}
+                    options={{
+                        headerShown: false,
+                    }}
+                />
+                <Stack.Screen
+                    name="navigateCard"
+                    component={RideOptionsCard}
+                    options={{
+                        headerShown: false,
+                    }}
+                />
+            </Stack.Navigator>
+
+        </View>
+    )
+}
+
+export default MapScreen
+
+const styles = StyleSheet.create({})
